84419228a960f1fd2039879fbf9e93e84be98b22
Justin Hunt <poodllsupport@gmail.com>
2016-05-01 10:05:48 -0400

    updated syntax

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2015111301;               //  The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2010031900;      // Requires this Moodle version
-$module->cron      = 0;               // Period for cron to check this module (secs)
-$module->component = 'mod_pairwork'; // To check on upgrade, that module sits in correct place
+$plugin->version   = 2015111301;               //  The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2010031900;      // Requires this Moodle version
+$plugin->cron      = 0;               // Period for cron to check this module (secs)
+$plugin->component = 'mod_pairwork'; // To check on upgrade, that module sits in correct place
